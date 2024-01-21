c9991c3a28b9d197abcee821d276a47409fb7a9b
Victorbp725 <Victorbp725@gmail.com>
2024-01-21 12:32:08 +0100

    JUEGO TERMINADO

--- piedra_papel_tijera.js
+++ piedra_papel_tijera.js
@@ -1,5 +1,9 @@
 const prompt = require("prompt-sync")({sigint: true});
 
+let puntosmaquina = 0;
+let puntoshumano = 0;
+let resultadofinal;
+
 // PROGRAMA
 let quienGana
 for (let i = 0 ; i < 3; i++){
@@ -8,6 +12,20 @@ for (let i = 0 ; i < 3; i++){
         console.log ("")
     }
 }
+
+console.log ('Resultado final -> Humano con ' + puntoshumano + ' puntos | Maquina con ' + puntosmaquina + ' puntos')
+
+//ERROR BOBO
+function eleccion (){
+    let eleccionusuario = prompt("Que opcion eliges? piedra, papel o tijeras?");
+    eleccionusuario = eleccionusuario.toLowerCase();
+    while (!(eleccionusuario === "piedra" || eleccionusuario === "papel"|| eleccionusuario === "tijeras")) {
+        console.log ("INGRESA UNA OPCION VALIDA") 
+        eleccionusuario = prompt("Que opcion eliges? piedra, papel o tijeras?");
+        eleccionusuario = eleccionusuario.toLowerCase();
+    }
+    return eleccionusuario
+} 
 
 
 // FUNCIONES
@@ -19,33 +37,32 @@ function jugar(){
     console.log(`La maquina ha elegido: ${maquina}`);
 
         //EL JUGADOR GANA SI 
-    if ((jugador === "papel" && maquina === "piedra") || (jugador === "piedra" && maquina === "tijeras") || (jugador === "tijeras" && maquina === "papel")){
-        console.log(`El humano gana! ${jugador} vence ${maquina}`);
-        return "maquina";
+    if ((jugador === "papel" && maquina ==="piedra") || (jugador ==="piedra" && maquina ==="tijeras") || (jugador ==="tijeras" && maquina ==="papel")){
+        console.log(`EL HUMANO GANA! ${jugador} vence ${maquina}`) 
+        puntoshumano++;
+        
+        
     }
        //SI ES UN EMPATE
     else if ((jugador === "piedra" && maquina === "piedra") || (jugador === "papel" && maquina === "papel") || (jugador === "tijeras" && maquina === "tijeras")) {
         console.log ("EMPATE");
     } else {
-        console.log(`La maquina gana! ${maquina} vence ${jugador}`);  
-        return 0;
+        console.log(`LA MAQUINA GANA! ${maquina} vence ${jugador}`);  
+        
+        puntosmaquina++;
     }
 }
-
-
-
-function eleccion (){
-    let eleccion = prompt("Que opcion eliges? piedra, papel o tijeras?");
-    eleccion = eleccion.toLowerCase();
-    while (!eleccion == "piedra" , "papel" , "tijera") {
-        console.log ("Ingresa una opcion valida") 
-    }
-    return eleccion;
-} 
-
-
 
 function eleccionMaquina() {
     const options = ["tijeras" , "piedra" , "papel"];
     return options[Math.floor(Math.random()* options.length)];
 }
+
+if (puntoshumano > puntosmaquina){
+    resultadofinal = 'Has ganado'
+}else if (puntoshumano < puntosmaquina){
+    resultadofinal = 'La maquina ha ganado'
+} else {
+    resultadofinal = 'HABEIS QUEDADO EMPATE :)'
+}
+console.log(resultadofinal);
